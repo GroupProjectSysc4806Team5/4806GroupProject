c27adf95630b028027008f22541078ba2bb5db87
Hussain <Hussainalbaidhani@cmail.carleton.ca>
2021-03-10 19:21:41 -0500

    was needed

--- GroupProject/src/main/java/team/GroupProject/Order.java
+++ GroupProject/src/main/java/team/GroupProject/Order.java
@@ -1,5 +1,45 @@
 package team.GroupProject;
+import java.util.List;
+
+import javax.persistence.*;
 
 public class Order {
+	
+    @Id
+    @GeneratedValue
+    private int id;
+    
+    @OneToMany(fetch=FetchType.EAGER, targetEntity = Book.class)
+    private List<Book> books;
+    private Customer customer;
+    
+    public Order(int id, List<Book> books, Customer customer) {
+    	
+        this.id = id;
+        this.books = books;
+        this.customer = customer;
+    }
+    
+    public int getId() { 
+    	return this.id; }
+    
+     public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+    public void setId(int id) {
+    	this.id = id; }
 
+    public List<Book> getBooks() { 
+    	return this.books; }
+    
+    //one customer could place multiple orders
+    @ManyToOne
+    public Customer getCustomer() {
+        return this.customer;
+    }
+    
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    
 }
