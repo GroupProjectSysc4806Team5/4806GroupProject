6ef29c9af4713c276a3d09f2592d99aa95b7818f
Jolar Tabungar <jolar_jjb24@hotmail.com>
2021-04-07 17:34:59 -0400

    Update unit tests and cart checkout integration test

--- src/test/java/Bookstore/Model/CartTest.java
+++ src/test/java/Bookstore/Model/CartTest.java
@@ -58,13 +58,23 @@ class CartTest {
 	void testCalculateTotal() {
 		Book book2 = new Book();
 		Book book3 = new Book();
+		book2.setId(1L);
+		book3.setId(2L);
 		book2.setPrice(31.00);
 		book3.setPrice(69.00);
+		book2.setQuantity(5);
+		book3.setQuantity(5);
+		book.setId(0L);
+
+		cart = new Cart();
 		cart.addBook(book);
 		cart.addBook(book2);
 		cart.addBook(book3);
 
-		assertEquals(cart.calculateTotal(), 31.00 + 69.00 + 25.00);
+		cart.increaseQuantity(book2.getId());
+		cart.increaseQuantity(book3.getId());
+
+		assertEquals(cart.calculateTotal(), 31.00 * 2 + 69.00 * 2 + 25.00);
 	}
 
 }
